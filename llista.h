97d877a85dcfe02f23ca0ab5eb263dedc1069f51
marccespedes <43566746+marccespedes@users.noreply.github.com>
2018-10-07 22:23:06 +0200

    Feat: Included the definition of functions of bidireccional list

--- llista.h
+++ llista.h
@@ -18,5 +18,16 @@ typedef struct {
 
 //Capçalera de les funcions
 LlistaBID LLISTABIDORD_crea();
-
+void LLISTABIDORD_insereixDavant(LlistaBID *l, int num);
+void LLISTABIDORD_insereixDarrera(LlistaBID *l, int num);
+int LLISTABIDORD_consulta(LlistaBID l);
+void LLISTABIDORD_esborra(LlistaBID *l);
+void LLISTABIDORD_avanca(LlistaBID * l);
+void LLISTABIDORD_retrocedeix(LlistaBID *l);
+int LLISTABIDORD_fi(LlistaBID l);
+int LLISTABIDORD_inici(LlistaBID l);
+void LLISTABIDORD_vesInici(LlistaBID *l);
+void LLISTABIDORD_vesFinal(LlistaBID *l);
+int LLISTABIDORD_buida(LlistaBID l);
+void LLISTABIDORD_destrueix(LlistaBID *l);
 #endif
